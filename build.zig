401bba992f927f414b6e9133365aa61baa822450
dbl-helix <jackblack@poppy>
2026-01-19 09:20:47 -0700

    web exports won't work (glsl 430)

--- build.zig
+++ build.zig
@@ -12,8 +12,6 @@ pub fn build(b: *std.Build) !void {
     const raylib = raylib_dep.module("raylib");
     const raygui = raylib_dep.module("raygui");
 
-    const raylib_artifact = raylib_dep.artifact("raylib");
-
     const build_metadata = b.createModule(.{
         .root_source_file = b.path("build.zig.zon"),
         .target = target,
@@ -33,50 +31,17 @@ pub fn build(b: *std.Build) !void {
 
     const run_step = b.step("run", "Run the app");
 
-    // web exports are completely separate
-    if (target.query.os_tag == .emscripten) {
-        const emsdk = rlz.emsdk;
-        const wasm = b.addLibrary(.{
-            .name = "final",
-            .root_module = exe_mod,
-        });
+    const exe = b.addExecutable(.{
+        .name = "final",
+        .root_module = exe_mod,
+        .use_lld = !((target.query.os_tag == .linux) or (target.query.os_tag == null)), // https://github.com/raylib-zig/raylib-zig/issues/219#issuecomment-2708936845
+    });
+    b.installArtifact(exe);
 
-        const install_dir: std.Build.InstallDir = .{ .custom = "web" };
-        const emcc_flags = emsdk.emccDefaultFlags(b.allocator, .{ .optimize = optimize });
-        const emcc_settings = emsdk.emccDefaultSettings(b.allocator, .{ .optimize = optimize });
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
 
-        const emcc_step = emsdk.emccStep(b, raylib_artifact, wasm, .{
-            .optimize = optimize,
-            .flags = emcc_flags,
-            .settings = emcc_settings,
-            .shell_file_path = emsdk.shell(raylib_dep.builder),
-            .install_dir = install_dir,
-            .embed_paths = &.{.{ .src_path = "resources/" }},
-        });
-        b.getInstallStep().dependOn(emcc_step);
-
-        const html_filename = try std.fmt.allocPrint(b.allocator, "{s}.html", .{wasm.name});
-        const emrun_step = emsdk.emrunStep(
-            b,
-            b.getInstallPath(install_dir, html_filename),
-            &.{},
-        );
-
-        emrun_step.dependOn(emcc_step);
-        run_step.dependOn(emrun_step);
-    } else {
-        const exe = b.addExecutable(.{
-            .name = "final",
-            .root_module = exe_mod,
-            .use_lld = !((target.query.os_tag == .linux) or (target.query.os_tag == null)), // https://github.com/raylib-zig/raylib-zig/issues/219#issuecomment-2708936845
-        });
-        b.installArtifact(exe);
-
-        const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        run_step.dependOn(&run_cmd.step);
-    }
+    run_step.dependOn(&run_cmd.step);
 }
 
 const std = @import("std");
